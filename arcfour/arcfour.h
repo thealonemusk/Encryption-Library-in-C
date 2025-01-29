e6a227f572a1c16950f53321712a7cdf92a759ad
thealonemusk <ashutoshj.cs.21@nitj.ac.in>
2025-01-30 00:01:29 +0530

    added the definition foe the rc4decrypt

--- arcfour/arcfour.h
+++ arcfour/arcfour.h
@@ -1,4 +1,3 @@
-/* arcfour.h */
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -7,29 +6,21 @@
 #include <assert.h>
 #include <errno.h>
 #define export  __attribute__((visibility("default")))
-#define rc4decrypt(x,y) rc4encrypt(x,y)
+#define rc4decrypt(rc4, data, size) rc4encrypt(rc4, data, size)
 #define rc4uninit(x) free(x)
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
-struct  s_arcfour
-{
-    int16 i,j,k; 
+struct s_arcfour {
+    int16 i, j, k;
     int8 s[256];
-
 };
 
 typedef struct s_arcfour Arcfour;
 
-
-
 export Arcfour *rc4init(int8*, int16);
 int8 rc4byte(Arcfour*);
 
-export int8 *rc4encrypt(Arcfour* , int8*, int16);
-
-
-
-
+export int8 *rc4encrypt(Arcfour*, int8*, int16);
